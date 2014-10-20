d01332702e12794c7c7d57e4b97d27452b6fc714
heqteyur <heqteyur@126.com>
2014-10-20 11:40:44 +0600

    ShangpinXinxi qoshuldi

--- KoklenSodigerLight/MainWindow.Designer.cs
+++ KoklenSodigerLight/MainWindow.Designer.cs
@@ -28,24 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.سىستېماToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تاۋارئۇچۇرىToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تاۋاركىرگۈزۈشToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تاۋارسېتىشToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ياردەمToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تاۋارئاساسىيئۇچۇرىToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("ALKATIP UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.سىستېماToolStripMenuItem,
+            this.تاۋارئۇچۇرىToolStripMenuItem,
+            this.تاۋاركىرگۈزۈشToolStripMenuItem,
+            this.تاۋارسېتىشToolStripMenuItem,
+            this.ياردەمToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1005, 31);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("ALKATIP UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStrip1.Location = new System.Drawing.Point(0, 31);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1005, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("ALKATIP UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Location = new System.Drawing.Point(0, 545);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1005, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // سىستېماToolStripMenuItem
+            // 
+            this.سىستېماToolStripMenuItem.Name = "سىستېماToolStripMenuItem";
+            this.سىستېماToolStripMenuItem.Size = new System.Drawing.Size(60, 27);
+            this.سىستېماToolStripMenuItem.Text = "سىستېما";
+            // 
+            // تاۋارئۇچۇرىToolStripMenuItem
+            // 
+            this.تاۋارئۇچۇرىToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تاۋارئاساسىيئۇچۇرىToolStripMenuItem});
+            this.تاۋارئۇچۇرىToolStripMenuItem.Name = "تاۋارئۇچۇرىToolStripMenuItem";
+            this.تاۋارئۇچۇرىToolStripMenuItem.Size = new System.Drawing.Size(83, 27);
+            this.تاۋارئۇچۇرىToolStripMenuItem.Text = "تاۋار ئۇچۇرى";
+            // 
+            // تاۋاركىرگۈزۈشToolStripMenuItem
+            // 
+            this.تاۋاركىرگۈزۈشToolStripMenuItem.Name = "تاۋاركىرگۈزۈشToolStripMenuItem";
+            this.تاۋاركىرگۈزۈشToolStripMenuItem.Size = new System.Drawing.Size(98, 27);
+            this.تاۋاركىرگۈزۈشToolStripMenuItem.Text = "تاۋار كىرگۈزۈش";
+            // 
+            // تاۋارسېتىشToolStripMenuItem
+            // 
+            this.تاۋارسېتىشToolStripMenuItem.Name = "تاۋارسېتىشToolStripMenuItem";
+            this.تاۋارسېتىشToolStripMenuItem.Size = new System.Drawing.Size(83, 27);
+            this.تاۋارسېتىشToolStripMenuItem.Text = "تاۋار سېتىش";
+            // 
+            // ياردەمToolStripMenuItem
+            // 
+            this.ياردەمToolStripMenuItem.Name = "ياردەمToolStripMenuItem";
+            this.ياردەمToolStripMenuItem.Size = new System.Drawing.Size(50, 27);
+            this.ياردەمToolStripMenuItem.Text = "ياردەم";
+            // 
+            // تاۋارئاساسىيئۇچۇرىToolStripMenuItem
+            // 
+            this.تاۋارئاساسىيئۇچۇرىToolStripMenuItem.Name = "تاۋارئاساسىيئۇچۇرىToolStripMenuItem";
+            this.تاۋارئاساسىيئۇچۇرىToolStripMenuItem.Size = new System.Drawing.Size(184, 28);
+            this.تاۋارئاساسىيئۇچۇرىToolStripMenuItem.Text = "تاۋار ئاساسىي ئۇچۇرى";
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 594);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1005, 567);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("ALKATIP UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "كۆكلەن سودىگەر يىنىك نۇسقىسى 1.0";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem سىستېماToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تاۋارئۇچۇرىToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تاۋاركىرگۈزۈشToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تاۋارسېتىشToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ياردەمToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تاۋارئاساسىيئۇچۇرىToolStripMenuItem;
     }
 }
 
